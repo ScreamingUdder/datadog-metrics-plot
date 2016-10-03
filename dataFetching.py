c5a0a9c30a48b24c09c8a4b25fcbd23acb8a9918
Matthew D Jones <matthew.d.jones@tessella.com>
2016-10-03 13:32:05 +0100

    also get bytes in and bytes out

--- dataFetching.py
+++ dataFetching.py
@@ -1,7 +1,6 @@
 from datadog import initialize, api
 import time
 import json
-import pandas
 
 options = {
     'api_key': '5d21d1ac7ccb1a6ca8a4a1e6986882ec', #Your API_KEY and APP_KEY, see https://app.datadoghq.com/account/settings#api
@@ -10,14 +9,26 @@ options = {
 
 initialize(**options)
 
-start = int(time.time()) - 300 #Here you can specify the time period over which you want to fetch the data, it's in seconds so here we fetch 5 mins
-end = start + 300
+end = int(time.time())
+start = end - 600 #Here you can specify the time period over which you want to fetch the data, it's in seconds so here we fetch 10 mins
 
 query = 'kafka.messages_in.rate{*}' #Select the metric you want to get, see your list here: https://app.datadoghq.com/metric/summary . Select the host from which you want the data, see here: https://app.datadoghq.com/infrastructure
 
-results = api.Metric.query(start=start - 300, end=end, query=query)
+results = api.Metric.query(start=start, end=end, query=query)
 
 f = open('out.txt', 'w') #this will create a file name out.txt in the folder you are in
 print >> f, json.dumps(results)
 f.close()
 #print results #That should display the results in the terminal
+
+query = 'kafka.net.bytes_in.rate{*}'
+results = api.Metric.query(start=start, end=end, query=query)
+f = open('bytes_in.txt', 'w') #this will create a file name out.txt in the folder you are in
+print >> f, json.dumps(results)
+f.close()
+
+query = 'kafka.net.bytes_out.rate{*}'
+results = api.Metric.query(start=start, end=end, query=query)
+f = open('bytes_out.txt', 'w') #this will create a file name out.txt in the folder you are in
+print >> f, json.dumps(results)
+f.close()
